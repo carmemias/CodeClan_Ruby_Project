c1a139660cb247906f7d34b646087235cf3f6cfc
carmemias <info@carmemias.com>
2019-02-01 13:00:39 +0000

    created initial merchants controller and views

---
+++ controllers/merchants_controller.rb
@@ -0,0 +1,33 @@
+require_relative("../models/merchants")
+
+# create
+get '/merchants/new' do
+  erb(:'merchants/new')
+end
+
+# save
+post '/merchants' do
+  @merchant = Merchant.new(params)
+  @merchant.save()
+  erb(:'merchants/index') #needs new method
+end
+
+# edit
+get '/merchants/:id/edit' do
+  @merchant = Merchant.find_by_id(params[:id].to_i) # needs new method
+  erb(:'merchants/edit')
+end
+
+# update
+post 'merchants/:id' do
+  @merchant = Merchant.new(params)
+  @merchant.update()
+  erb(:'/merchants/show') # needs new method
+end
+
+# delete
+post '/merchants/:id/delete' do
+  merchant = Merchant.find_by_id(params[:id].to_i) # needs new method
+  merchant.delete() if merchant
+  redirect to(:'/merchants/index')
+end
