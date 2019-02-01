11ee98ebefccc8fa13dab3b5458116b5f0087b84
carmemias <info@carmemias.com>
2019-02-01 11:16:08 +0000

    Added create, edit and delte methods to class Merchant

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require("pry")
 
 require_relative("../models/tag")
+require_relative("../models/merchant")
 
 @tag1 = Tag.new({"name" => "groceries"})
 @tag2 = Tag.new({"name" => "public transport"})
@@ -8,7 +9,12 @@ require_relative("../models/tag")
 
 [@tag1, @tag2, @tag3].each{ |tag| tag.save() }
 
+@merchant1 = Merchant.new({"name" => "Morrisons"})
+@merchant2 = Merchant.new({"name" => "Boots"})
+@merchant3 = Merchant.new({"name" => "SPT"})
+@merchant4 = Merchant.new({"name" => "Cineworld"})
 
+[@merchant1, @merchant2, @merchant3, @merchant4].each{ |merchant| merchant.save() }
 
 binding.pry
 nil
--- models/merchant.rb
+++ models/merchant.rb
@@ -8,4 +8,31 @@ class Merchant
     @name = options['name']
     @id = options['id'].to_i() if options['id']
   end
+
+  # create
+  def save()
+    sql = "INSERT INTO merchants (name) VALUES ($1) RETURNING id;"
+    values = [@name]
+    returned_id = SqlRunner.run(sql, values)[0]['id'].to_i()
+    @id = returned_id
+  end
+
+  # edit
+  def update()
+    sql = "UPDATE merchants SET name = $2 WHERE id = $1;"
+    values = [@id, @name]
+    SqlRunner.run(sql, values)
+  end
+
+  # delete
+  def delete()
+    sql = "DELETE FROM merchants WHERE id = $1;"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM merchants;"
+    SqlRunner.run(sql)
+  end
 end
