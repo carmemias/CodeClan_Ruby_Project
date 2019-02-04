7688ae5c6772345b581efa471d99aa3de27b4f3d
carmemias <info@carmemias.com>
2019-02-04 10:25:30 +0000

    Added new table user_settings and added time field to transactions table

--- db/spending_tracker.sql
+++ db/spending_tracker.sql
@@ -3,6 +3,7 @@
 DROP TABLE IF EXISTS transactions;
 DROP TABLE IF EXISTS merchants;
 DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS user_settings;
 
 CREATE TABLE tags (
 	id serial8 PRIMARY KEY,
@@ -17,6 +18,14 @@ CREATE TABLE merchants (
 CREATE TABLE transactions (
 	id serial8 PRIMARY KEY,
 	amount INT4,
+	time TIMESTAMP,
 	tag_id INT8 REFERENCES tags(id) ON DELETE CASCADE,
 	merchant_id INT8 REFERENCES merchants(id)
 );
+
+CREATE TABLE user_settings (
+	id serial8 PRIMARY KEY,
+	first_name VARCHAR(255),
+	last_name VARCHAR(255),
+	budget INT4
+)
